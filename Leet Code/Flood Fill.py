e71c13149836653fb044c666d9bd74e23b62d487
elliot-98 <80590814+elliot-98@users.noreply.github.com>
2021-12-08 21:52:39 +0100

    Create Flood Fill.py

---
+++ Leet Code/Flood Fill.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+
+        seen = set()
+        m = len(image)
+        n = len(image[0])
+        oldColor = image[sr][sc]
+        
+        def fill(sr,sc) : 
+        
+            if ((sr,sc) not in seen) & (0<=sr<m) & (0<=sc<n) :
+                if image[sr][sc] == oldColor : 
+                    image[sr][sc] = newColor
+                    seen.add((sr,sc))
+                    fill(sr+1,sc)
+                    fill(sr,sc+1)
+                    fill(sr-1,sc)
+                    fill(sr,sc-1)
+        fill(sr,sc)
+        return image
