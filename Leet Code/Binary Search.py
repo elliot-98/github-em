2d10b72da85ae3c1d9c40f2aaaba87e5401934f5
Elliot Muller <emuller@air-de-elliot.home>
2023-11-28 23:43:03 +0100

    formatted file

--- Leet Code/Binary Search.py
+++ Leet Code/Binary Search.py
@@ -5,26 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
-        cpt1=0
-        cpt2=0
-        while len(nums)>1:
-            l=len(nums)
-            nums1 = nums[:l//2]
-            nums2 = nums[l//2:]
-            if nums1[-1]<target and nums2[0]>target: 
+
+        cpt1 = 0
+        cpt2 = 0
+        while len(nums) > 1:
+            l = len(nums)
+            nums1 = nums[: l // 2]
+            nums2 = nums[l // 2 :]
+            if nums1[-1] < target and nums2[0] > target:
                 return -1
-            elif nums1[-1]>target:
-                nums=nums1
-            elif nums2[0]<target:
-                cpt2+=l//2
-                cpt1+=l//2
-                nums=nums2
-            elif nums1[-1]==target:
-                return cpt1 + l//2-1
+            elif nums1[-1] > target:
+                nums = nums1
+            elif nums2[0] < target:
+                cpt2 += l // 2
+                cpt1 += l // 2
+                nums = nums2
+            elif nums1[-1] == target:
+                return cpt1 + l // 2 - 1
             else:
-                return cpt2 + l//2
-        if nums[0]==target:
+                return cpt2 + l // 2
+        if nums[0] == target:
             return cpt2
         else:
             return -1
